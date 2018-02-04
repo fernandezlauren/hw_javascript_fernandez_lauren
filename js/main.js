dc88eae884240a351bdd16efdd5e1f261e0520c2
Lauren Fernandez <lfernan60@ufl.edu>
2018-02-04 18:34:07 -0500

    Add JS Object about presidents.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,46 @@
 var iceCream = ["Chocolate", "Vanilla", "Rocky Road"];
 iceCream.push("Strawberry");
+
+var pres1 = {
+  firstName:"Donald",
+  lastName:"Trump",
+  termLength:4,
+  party:"Republican",
+  yearsOfPresidency:1
+};
+
+var pres2 = {
+  firstName:"Barack",
+  lastName:"Obama",
+  termLength:4,
+  party:"Democratic",
+  yearsOfPresidency:8
+};
+
+var pres3 = {
+  firstName:"George W.",
+  lastName:"Bush",
+  termLength:4,
+  party:"Republican",
+  yearsOfPresidency:8
+};
+
+var pres4 = {
+  firstName:"Bill",
+  lastName:"Clinton",
+  termLength:4,
+  party:"Democratic",
+  yearsOfPresidency:8
+};
+
+var pres5 = {
+  firstName:"George H. W.",
+  lastName:"Bush",
+  termLength:4,
+  party:"Republican",
+  yearsOfPresidency:4
+};
+
+var presidents = [pres1, pres2, pres3, pres4, pres5];
+
+console.log(presidents[2]["firstName"] + " " + presidents[2]["lastName"]);

540e752eda45968f8e9d7b3bf137c461f25953c8
Lauren Fernandez <lfernan60@ufl.edu>
2018-02-04 18:36:44 -0500

    Add JS function titled howdy

--- js/main.js
+++ js/main.js
@@ -44,3 +44,30 @@ var pres5 = {
 var presidents = [pres1, pres2, pres3, pres4, pres5];
 
 console.log(presidents[2]["firstName"] + " " + presidents[2]["lastName"]);
+
+function howdy() {
+    console.log("Lauren Fernandez says hi!");
+}
+
+howdy();
+
+var numba = function(string) {
+    var length = string.length;
+    if (length < 7) console.log("What a short little word!");
+    else if (length > 7) console.log("I’m sorry, but that’s too many to count.");
+    else console.log("7, what a perfect choice!");
+};
+
+numba("7");
+numba("seventy");
+numba("university");
+
+function inception(display, favMovie) {
+    display(favMovie);
+}
+
+function display(movie) {
+    console.log(movie + " is a fantastic movie.");
+}
+
+inception(display, "Inception");

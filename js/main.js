d3febd6092bd17e35148c4e9eb615bdfe41aae21
Lauren Fernandez <lfernan60@ufl.edu>
2018-02-04 18:38:20 -0500

    Add function named inception.

--- js/main.js
+++ js/main.js
@@ -62,6 +62,23 @@ numba("7");
 numba("seventy");
 numba("university");
 
+function howdy() {
+    console.log("Lauren Fernandez says hi!");
+}
+
+howdy();
+
+var numba = function(string) {
+    var length = string.length;
+    if (length < 7) console.log("What a short little word!");
+    else if (length > 7) console.log("I’m sorry, but that’s too many to count.");
+    else console.log("7, what a perfect choice!");
+};
+
+numba("7");
+numba("seventy");
+numba("university");
+
 function inception(display, favMovie) {
     display(favMovie);
 }
